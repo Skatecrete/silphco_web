efc0e577592de1976f3fcb42c55d4b709b1365b2
Skatecrete <skatecrete@gmail.com>
2026-09-07 14:29:57 -0400

    Update imageUrls.ts

--- src/utils/imageUrls.ts
+++ src/utils/imageUrls.ts
@@ -12,7 +12,7 @@ export const DIAL_TILES = {
   services: `${BASE_WEB_URL}/dial_tiles/dial_services.png`,
   orderCart: `${BASE_WEB_URL}/dial_tiles/dial_ordercart.png`,
   history: `${BASE_WEB_URL}/dial_tiles/dial_history.png`,
-  chat: '/images/dial_chat.png',
+  chat: `${BASE_WEB_URL}/dial_tiles/dial_chat.png`, 
   admin: `${BASE_WEB_URL}/dial_tiles/dial_admin.png`,
   logout: `${BASE_WEB_URL}/dial_tiles/dial_logout.png`,
 };
